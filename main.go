149259d30d6471ec5f5026348c22fda933488fd1
邹优贵 <337196325@qq.com>
2022-03-13 15:59:50 +0800

    添加忽略文件

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	MongodbObj := &models.Mongodb{}
 	go MongodbObj.NewMongo()
 
-	// orm.RegisterDataBase("default", "mysql", "root:123456@tcp(127.0.0.1:3306)/beego?charset=utf8")
+	//orm.RegisterDataBase("default", "mysql", "root:123456@tcp(127.0.0.1:3306)/beego?charset=utf8")
 
 	// go models.Consumers()
 	// models.Producer()
