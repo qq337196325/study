6c54f377ff8a8a4da1295ae6b59be72168dacfc1
邹优贵 <337196325@qq.com>
2022-03-13 16:16:44 +0800

    添加忽略文件

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"study/models"
 	_ "study/routers"
 
+	"github.com/beego/beego/v2/client/orm"
 	beego "github.com/beego/beego/v2/server/web"
 )
 
@@ -11,7 +12,7 @@ func main() {
 	MongodbObj := &models.Mongodb{}
 	go MongodbObj.NewMongo()
 
-	//orm.RegisterDataBase("default", "mysql", "root:123456@tcp(127.0.0.1:3306)/beego?charset=utf8")
+	orm.RegisterDataBase("default", "mysql", "root:123456@tcp(127.0.0.1:3306)/beego?charset=utf8")
 
 	// go models.Consumers()
 	// models.Producer()
